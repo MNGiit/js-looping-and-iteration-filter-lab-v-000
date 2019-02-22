df3cc65a10d839664fe50f56cd4b944c1f5ad7b8
Mohammed Noori <kmanoorib@gmail.com>
2019-02-22 23:39:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 // Code your solution in this file
-
-
-
-
-
-findMatching()
-returns all drivers that match the passed in name ‣
-ReferenceError: findMatching is not defined
-    at Context.<anonymous> (test/indexTest.js:8:7)const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
-expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
+function findMatching(drivers) {
+  return drivers;
+}
